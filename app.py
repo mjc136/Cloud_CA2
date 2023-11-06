8ff646d88b2b13cde2d5a1b3d98120bd999b7122
mjc136 <mjcullen2015@gmail.com>
2023-11-06 19:28:54 +0000

    updated methods with my functions

--- app.py
+++ app.py
@@ -9,21 +9,34 @@ import my_utils
 
 FOLDER = r"swimdata/"
 
-NAMES = my_utils.getNames(FOLDER)
-SORTED_NAMES = sorted(NAMES)
-
 app = Flask(__name__)
 
 
 @app.get("/")
-@app.get("/hello")
-def hello():
-    return "Hello from my first web app - cool, isn't it?"  # ANY string.
+@app.get("/getswimmers")
+def get_swimmers_names():
+    NAMES = my_utils.getNames(FOLDER)
+    SORTED_NAMES = sorted(NAMES)
+    return render_template(
+        "select.html",
+        title = "Select a swimmer to chart",
+        data = SORTED_NAMES,
+    )
 
-@app.get("/swimmers")
-def displaySwimmers():
-    ...
-
+@app.post("/displayevents")
+def list_swimmer_events(): # get all events from a swimmer
+    swimmers_event = set()
+    for filename in os.listdir(FOLDER):
+        result = swim_utils.get_swimmers_data(filename)
+        event = result[2] + " " + result[3]
+        #if name in filename:
+        swimmers_event.add(event)
+    return render_template(
+        "select.html",
+        title="Select a swimmers event to chart",
+        data = swimmers_event,
+    )
+    
 
 @app.get("/chart")
 def display_chart():
@@ -39,7 +52,7 @@ def display_chart():
 
     the_title = f"{name} (Under {age}) {distance} {stroke}"
     from_max = max(converts) + 50
-    the_converts = [ hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts ]
+    the_converts = [hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts ]
 
     the_data = zip(the_converts, the_times)
 
@@ -51,23 +64,8 @@ def display_chart():
     )
 
 
-@app.get("/getswimmers")
-def get_swimmers_names():
-    files = os.listdir(swim_utils.FOLDER)
-    files.remove(".DS_Store")
-    names = set()
-    for swimmer in files:
-        names.add(swim_utils.get_swimmers_data(swimmer)[0])
-    return render_template(
-        "select.html",
-        title="Select a swimmer to chart",
-        data=sorted(names),
-    )
 
 
-@app.post("/displayevents")
-def get_swimmer_events():
-    return request.form["swimmer"]
 
 
 if __name__ == "__main__":
